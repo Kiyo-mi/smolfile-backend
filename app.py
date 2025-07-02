32ea92cb8e03fea14b1975270bab447863dde517
Kiyo-mi <losfrancis98@gmail.com>
2025-07-01 20:13:20 -0400

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -72,4 +72,7 @@ def compress():
 
 # Run the app locally
 if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
